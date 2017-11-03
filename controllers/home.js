4595d28b6da5e0d4748894554fad035f850f9f66
samueljim <samueljim@msn.com>
2017-11-03 16:05:37 +1000

    dropdown box and atempt at pull of more info

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,7 @@
-
 const Pokedex = require('pokedex-promise-v2');
-
+const recrawler = require('recrawler');
 const P = new Pokedex();
 const Pokemon = require('../models/Pokemon');
-
 // const cheerio = require('cheerio');
 
 /**
@@ -11,7 +9,9 @@ const Pokemon = require('../models/Pokemon');
  * Home page.
  */
 exports.index = (req, res, next) => {
-  Pokemon.find({ versions: 'red' }, (err, pokies) => {
+  Pokemon.find({
+    versions: 'red'
+  }, (err, pokies) => {
     if (err) {
       res.redirect('/');
       return next(err);
@@ -28,72 +28,74 @@ exports.index = (req, res, next) => {
  * find's a pokemon
  */
 exports.getPokemon = (req, res, next) => {
-  req.assert('game', 'Not a pokemon game').isIn(["white-2", "black-2", "white", "black", "soulsilver", "heartgold", "platinum", "pearl",  "diamond", "leafgreen",  "firered",  "emerald", "sapphire", "ruby", "crystal", "silver", "gold", "yellow", "blue", "red"]);
-
-
+  req.assert('game', 'Not a pokemon game').isIn(["white-2", "black-2", "white", "black", "soulsilver", "heartgold", "platinum", "pearl", "diamond", "leafgreen", "firered", "emerald", "sapphire", "ruby", "crystal", "silver", "gold", "yellow", "blue", "red"]);
   const errors = req.validationErrors();
 
   if (errors) {
     req.flash('errors', errors);
-    // return res.redirect('/');
+    return res.redirect('/');
   }
   console.log(req.body.game);
 
-  // res.redirect('/');
-
-  Pokemon.find({ versions: req.body.game }, (err, pokies) => {
+  Pokemon.find({
+    versions: req.body.game
+  }, (err, pokies) => {
     if (err) {
       res.redirect('/');
       return next(err);
     }
     // console.log(req.body.game);
     res.render('home', {
-      title: 'Home',
-      pokies
+      title: req.body.game,
+      pokies,
+      version: req.body.game,
     });
   });
 };
 
 exports.fillPokemon = (req, res, next) => {
   console.log('filling');
-  var i = 0;
-  for (i = 0; i < 802; i++) {
+  // var i = 0;
+  for (var i = 0; i < 1; i++) {
     P.getPokemonByName(1 + i)
-    .then(function(response) {
-      var games = [];
-      for (game in response.game_indices) {
-        games.push(response.game_indices[game].version.name);
-      }
-      // console.log(games);
-      
-      const pokemon = new Pokemon({
-        versions: games,
-        name: response.name,
-        height: response.height,
-        weight: response.weight,
-        id: response.id,
-        order: response.order,
-        location_area_encounters: response.location_area_encounters,
-        sprite: response.sprites.front_default,
-        sprite2: response.sprites.back_default
+      .then(function (response) {
+        var games = [];
+        for (game in response.game_indices) {
+          games.push(response.game_indices[game].version.name);
+        }
+        console.log('https://bulbapedia.bulbagarden.net/wiki/' + response.name);
+
+        recrawler('https://bulbapedia.bulbagarden.net/wiki/' + response.name)
+        .then($ => {
+            $('.roundy').each(function () {
+                const url = $(this).attr('a')
+                console.log(url)
+            })
+        })
+{/* <td class="roundy" style="background:#FFF; padding-left: 5px;"> <a href="/wiki/Starter_Pok%C3%A9mon" title="Starter Pokémon">Starter Pokémon</a> from <a href="/wiki/Professor_Oak" title="Professor Oak">Professor Oak</a> in <a href="/wiki/Pallet_Town" title="Pallet Town">Pallet Town</a>
+</td> */}
+        // const pokemon = new Pokemon({
+        //   versions: games,
+        //   name: response.name,
+        //   height: response.height,
+        //   weight: response.weight,
+        //   id: response.id,
+        //   order: response.order,
+        //   location_area_encounters: response.location_area_encounters,
+        //   sprite: response.sprites.front_default,
+        //   sprite2: response.sprites.back_default
+        // });
+
+      //   pokemon.save((err) => {
+      //     if (err) {
+      //       req.flash('errors', err);
+      //       return next(err);
+      //     }
+      //     console.log(response.id);
+      //   });
+      }).catch(function (error) {
+        console.log('There was an ERROR: ', error);
+        return next(error);
       });
-      
-      pokemon.save((err) => {
-        if (err) {
-            req.flash('errors', err);
-            return next(err);
-          }
-          // res.redirect('/');
-          console.log(response.id);
-        });
-    }).catch(function(error) {
-      console.log('There was an ERROR: ', error);
-      return next(error);
-    });
-    // // req.flash('success', pokemon);
-    // // res.redirect('/');
-    // res.send('done');
-    // });
   }
-  // console.log('done');
 };
